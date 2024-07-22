74191a19f7c6634712dc3464e6e9a447d19ac78c
kai <k.penazzi@web.de>
2024-07-22 22:23:45 +0200

    add layouts + Add Tasks

--- src/app/TasksApp.py
+++ src/app/TasksApp.py
@@ -3,6 +3,7 @@ from kivy.properties import ObjectProperty
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
 
 
 Builder.load_file('app/TasksApp.kv')
@@ -12,8 +13,16 @@ class TasksApp(Widget):
     input_add = ObjectProperty(None)
     layout_stack = ObjectProperty(None)
 
+    def btn_done_click(self, instance):
+        self.layout_stack.remove_widget(instance.parent)
+
     def btn_add_click(self):
-        label = Label(text=self.input_add.text, width=self.width, height=10, size_hint=(None, None), color=(245,17,150, 1))
+        container = BoxLayout(orientation='horizontal', width=self.width, size_hint=(1, .07))
+        label = Label(text=self.input_add.text, size_hint=(.8, 1), color=(245,17,150, 1))
+        btn = Button(text='done', size_hint=(.2, 1), on_press=self.btn_done_click)
+        container.add_widget(label)
+        container.add_widget(btn)
+
+        self.layout_stack.add_widget(container)
         self.input_add.text = ""
 
-        self.layout_stack.add_widget(label)
